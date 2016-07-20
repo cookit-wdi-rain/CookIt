10a0d4ec3482ec886737ce04358a04766008024b
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-07-20 16:27:20 -0400

    added models

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 'use strict'
 
-const env         = process.env.NODE_ENV || 'development';
-const DEV         = env==='development';
-const dotenv      = (DEV) ? require('dotenv').config() : undefined;
+// const env         = process.env.NODE_ENV || 'development';
+// const DEV         = env==='development';
+// const dotenv      = (DEV) ? require('dotenv').config() : undefined;
 
 const express     = require('express')
 const morgan      = require('morgan')
 const path        = require('path')
 const bodyParser  = require('body-parser')
 const app         = express()
-const userRoute   = require('./routes/user_route')
-const pantryRoute = require('./routes/pantry_route')
-const recipeRoute = require('./routes/recipe_route')
+// const userRoute   = require('./routes/user_route')
+// const pantryRoute = require('./routes/pantry_route')
+// const recipeRoute = require('./routes/recipe_route')
 const port        = process.argv[2] || process.env.PORT || 3000
 
-app.use(morgan( DEV ? 'dev' : 'common'))
+// app.use(morgan( DEV ? 'dev' : 'common'))
+app.use(morgan('dev'))
+
 app.use(bodyParser.json());
 
-app.use('/users', userRoute)
-app.use(express.static(path.join(__dirname,'dist')))
+// app.use('/users', userRoute)
+// app.use(express.static(path.join(__dirname,'dist')))
 
-app.use('/recipes', recipeRoute)
-app.use(express.static(path.join(__dirname,'dist')))
+// app.use('/recipes', recipeRoute)
+// app.use(express.static(path.join(__dirname,'dist')))
 
-app.use('/pantry', pantryRoute)
-app.use(express.static(path.join(__dirname,'dist')))
+// app.use('/pantry', pantryRoute)
+// app.use(express.static(path.join(__dirname,'dist')))
 
 app.listen(port, ()=>{
   console.log('Server maxin\' and relaxin\' at ', port)
