a4334677846a2458556fa9e72d045792da5a5b3f
Arlen Cho <arlen_cho@hotmail.com>
2016-07-21 18:49:46 -0400

    getting on same page

--- src/client/helpers/ajaxAdapter.js
+++ src/client/helpers/ajaxAdapter.js
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
-let myInit = {
- method: 'GET',
- headers: {
-   "X-Mashape-Key":
- }
-=======
 // let myInit = {
 //   method: 'GET'
 //   // headers: {
 //   //   "X-Mashape-Key":
 //  }
->>>>>>> 909865f138391b9492933f9ab76250f7fd89020f
+
 
 const ajaxAdapter = {
 
 cuisineCall(query) {
-<<<<<<< HEAD
- }
- let url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?cuisine='+ query
- return fetch(url, myInit)
- .then(res => res.json() )
-=======
+
 
   let url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?cuisine='+ query
   return fetch(url, myInit)
@@ -37,7 +25,7 @@ ingredientsCall(query) {
   let url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients='+ query
   return fetch(url, myInit)
   .then(res => res.json() )
->>>>>>> 909865f138391b9492933f9ab76250f7fd89020f
+
 },
 
 testCall() {
@@ -45,42 +33,22 @@ testCall() {
  .then(res=>res.json())
 },
 
-<<<<<<< HEAD
 
-ingredientsCall(query) {
- let url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients='+ query
- return fetch(url, myInit)
- .then(res => res.json() )
-},
-
-
-recipeCall(query) {
- let url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/'+ query + '/information'
- return fetch(url, myInit)
- .then(res => res.json() )
-=======
 recipeCall(query) {
   let url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/'+ query + '/information'
   return fetch(url, myInit)
   .then(res => res.json() )
->>>>>>> 909865f138391b9492933f9ab76250f7fd89020f
+
 },
 
 
 secondCall(query) {
-<<<<<<< HEAD
- return fetch('/testapi/'+query)
- .then(res=>res.json())
-}
-
-
-=======
   return fetch('/testapi/'+query)
   .then(res=>res.json())
 }
 
 }
->>>>>>> 909865f138391b9492933f9ab76250f7fd89020f
+
 //   getRecipes(){
 //     return fetch('/recipes')
 //       .then( r=> r.json() )
@@ -107,11 +75,8 @@ secondCall(query) {
 //     }).then( r=> r.json() )
 //   },
 
-<<<<<<< HEAD
-}
-export default ajaxAdapter;
-=======
+
 
 export default ajaxAdapter;
 
->>>>>>> 909865f138391b9492933f9ab76250f7fd89020f
+
