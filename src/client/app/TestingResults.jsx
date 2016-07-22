f9c9f963f63bd30af2aeaf54608674ad33b45522
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-07-22 13:48:26 -0400

    pulling in CB's work

--- src/client/app/TestingResults.jsx
+++ src/client/app/TestingResults.jsx
@@ -3,38 +3,39 @@ import React     from 'react'
 
 export default class Results extends React.Component {
   render(){
-    if(!this.state.selected){
+    // if(!this.state.selected){
+    //   return (
+    //     <div className="recipe_results">
+    //       {this.props.recipes.map((recipe,i)=>{
+    //         return(
+    //           <div className="image_container" key={i}>
+    //             <h4>{recipe.title}</h4>
+    //               <img className="recipe_image" src={recipe.img} />
+    //               <button value={recipe.main_id} onClick={this.props.onSelectRecipe}>Save</button>
+
+
+    //           </div>
+    //           )
+    //       })}
+    //     </div>
+    //   )
+    // } else {
       return (
         <div className="recipe_results">
           {this.props.recipes.map((recipe,i)=>{
             return(
               <div className="image_container" key={i}>
                 <h4>{recipe.title}</h4>
+                  <button
+                  value={recipe.main_id}
+                  onClick={this.props.onSelectRecipe}>
                   <img className="recipe_image" src={recipe.img} />
-                  <button value={recipe.main_id} onClick={this.props.onSelectRecipe}>Save</button>
-
-
+                  </button>
               </div>
               )
           })}
         </div>
       )
-    } else {
-      return (
-        <div className="recipe_results">
-          {this.props.recipes.map((recipe,i)=>{
-            return(
-              <div className="image_container" key={i}>
-                <h4>{recipe.title}</h4>
-                  <img className="recipe_image" src={recipe.img} />
-                  <button value={recipe.main_id} onClick={this.props.onSelectRecipe}>Save</button>
-
-
-              </div>
-              )
-          })}
-        </div>
-      )
-    }
+    // }
   }
 }
