ce01087bb8af7fab18797a1cc2398633270556c7
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-07-22 17:32:28 -0400

    ajax calls and second ajax calls work, sans getting ingredients array

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -2,7 +2,7 @@ import React              from 'react'
 import ReactDOM           from 'react-dom'
 import SearchContainer    from './SearchContainer.jsx'
 // import ajax               from '../helpers/ajaxAdapter.js'
-import SearchContainerTest from './SearchContainerTest.jsx'
+
 export default class App extends React.Component{
 
 
@@ -10,7 +10,7 @@ export default class App extends React.Component{
         return(
 
             <div>
-            <SearchContainerTest />
+            <SearchContainer />
             </div>
         )
     }
