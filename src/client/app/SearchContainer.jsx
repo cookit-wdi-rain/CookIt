fed2a7c76cb649c4991a8e56f0092307cd28b2f5
Chandler Basso <cebasso@asu.edu>
2016-07-23 13:50:35 -0400

    updated errors

--- src/client/app/SearchContainer.jsx
+++ src/client/app/SearchContainer.jsx
@@ -1,7 +1,7 @@
 import React          from 'react'
 import Search         from './Search.jsx'
 
-import Results        from './TestingResults.jsx'
+import Results        from './Results.jsx'
 import ResultsSelected from './ResultsSelected.jsx'
 import DropTest       from './Droptest.jsx'
 
@@ -35,37 +35,37 @@ export default class SearchContainer extends React.Component {
     })
   }
   handleSubmitSearch(event){
-    event.preventDefault();
-    console.log(this.state.dropdown)
-   if(this.state.dropdown ==="cuisine"){
-    ajax.cuisineCall(this.state.query).then( cuisine =>{
-    // ajax.testCall(this.state.query).then( cuisine =>{
-    //console.log(this.state.query)
-    console.log("Got back cuisine ", cuisine)
-      this.setState({
-        results: cuisine.results,
-        // results:cuisine,
-        dropdown:this.state.dropdown,
-        selected: false,
-        query:"",
-        searched: true
-      })
-    })
-    }
-    if(this.state.dropdown === "ingredient"){
-       // ajax.ingredientsCall(this.state.query).then( ingredient =>{
-      ajax.testCall(this.state.query).then( ingredient =>{
-    //console.log(this.state.query)
-    console.log(ingredient)
-      this.setState({
-        results: ingredient,
-        dropdown:this.state.dropdown,
-        query:"",
-        searched: true
-      })
-    })
-    }
-  }
+   event.preventDefault();
+   console.log(this.state.dropdown)
+  if(this.state.dropdown ==="cuisine"){
+   ajax.cuisineCall(this.state.query).then( cuisine =>{
+   // ajax.testCall(this.state.query).then( cuisine =>{
+   //console.log(this.state.query)
+   console.log("Got back cuisine ", cuisine)
+     this.setState({
+       results: cuisine.results,
+       // results:cuisine,
+       dropdown:this.state.dropdown,
+       selected: false,
+       query:"",
+       searched: true
+     })
+   })
+   }
+   if(this.state.dropdown === "ingredient"){
+      ajax.ingredientsCall(this.state.query).then( ingredient =>{
+     // ajax.testCall(this.state.query).then( ingredient =>{
+   //console.log(this.state.query)
+   console.log("ingredients",ingredient)
+     this.setState({
+       results: ingredient,
+       dropdown:this.state.dropdown,
+       query:"",
+       searched: true
+     })
+   })
+   }
+ }
 
  selectRecipe(event){
     event.preventDefault();
